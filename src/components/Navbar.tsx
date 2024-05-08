f1f2fe886a9d567dfdeb966278f60991eced628d
vrajdesai78 <vrajdesai78@gmail.com>
2024-05-08 21:04:31 +0530

    fix: navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,26 +5,27 @@ import toast from "react-hot-toast";
 
 const Navbar = () => {
   const pathname = usePathname();
+  console.log(pathname);
 
   return (
     <div className='w-full flex justify-between items-center px-6 max-w-[1200px]'>
       {/* Farview */}
       <Link href={"/"} className='text-2xl font-semibold text-[#7F5FC6]'>
-        Farview
+        Farview.id
       </Link>
 
       {/* share */}
       {pathname !== "/" && (
         <a className='w-24 h-11 px-6 py-3 bg-white rounded-full border border-slate-200 justify-center items-center gap-2.5 inline-flex'>
-          <button
+          <Link
             className='text-center text-slate-500 text-base font-normal tracking-tight'
-            onClick={async () => {
-              await navigator.clipboard.writeText(window.location.href);
-              toast.success("Link copied to clipboard");
-            }}
+            href={`https://warpcast.com/~/compose?embeds[]=https://www.farview.id/frames?fname=${pathname?.slice(
+              1
+            )}`}
+            target='_blank'
           >
             Share
-          </button>
+          </Link>
         </a>
       )}
     </div>
