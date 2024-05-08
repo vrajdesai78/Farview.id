ebc5272fbbb150401cb67453be3e06ba9e582bf8
tushar <117444846+tusharverma06@users.noreply.github.com>
2024-05-08 20:23:38 +0530

    style: changed heading position and allignment

--- src/components/Grid/Grid.tsx
+++ src/components/Grid/Grid.tsx
@@ -11,8 +11,8 @@ const Grid = ({ heading, children }: GridProps) => {
       className={`bg-white border relative border-slate-300 rounded-3xl flex flex-col  items-center ${
         !heading.includes("Top Base NFTs")
           ? !heading.includes("Tokens")
-            ? "md:h-[326px] lg:h-[287px] p-8"
-            : "h-[188px] w-full p-8"
+            ? "md:h-[326px] lg:h-[287px] p-8 !pt-5"
+            : "h-[188px] w-full p-8 !pt-6"
           : "md:h-[188px] lg:w-[calc(60%)] md:w-auto w-full md:p-8 p-4 justify-end"
       } ${
         heading.includes("Caster") || heading.includes("Follower")
@@ -24,13 +24,14 @@ const Grid = ({ heading, children }: GridProps) => {
           ? "justify-end gap-8 pb-[18px]"
           : " justify-between  gap-6"
       }
+      ${heading.includes("Caster") ? "!justify-start gap-4" : ""}
       `}
     >
-      {children}
-
-      <span className='text-center text-slate-500 text-sm lg:text-base font-normal tracking-tight'>
+      <span className="text-center text-slate-500 text-sm lg:text-base font-normal tracking-tight">
         {heading}
       </span>
+
+      {children}
     </div>
   );
 };
