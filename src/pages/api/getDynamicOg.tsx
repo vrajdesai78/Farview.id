cdd7442fcee712320066dffd4d899f13cd7606d1
vrajdesai78 <vrajdesai78@gmail.com>
2024-05-08 14:44:24 +0530

    fix: build error

--- src/pages/api/getDynamicOg.tsx
+++ src/pages/api/getDynamicOg.tsx
@@ -38,12 +38,12 @@ export default async function GET(request: NextRequest) {
     return new ImageResponse(
       (
         <div
-          tw=" w-full h-full p-10 pt-20  relative flex flex-col items-center justify-start bg-black"
+          tw=' w-full h-full p-10 pt-20  relative flex flex-col items-center justify-start bg-black'
           style={{ gap: "5px" }}
         >
-          <div tw=" w-full  relative flex items-center justify-between">
+          <div tw=' w-full  relative flex items-center justify-between'>
             <div
-              tw=" w-1/2 h-full  relative flex flex-col items-start justify-start max-w-1/2 "
+              tw=' w-1/2 h-full  relative flex flex-col items-start justify-start max-w-1/2 '
               style={{
                 gap: "10px",
               }}
@@ -52,15 +52,15 @@ export default async function GET(request: NextRequest) {
                 style={{
                   borderRadius: "9999px",
                 }}
-                tw="w-20 h-20  flex items-center justify-center"
+                tw='w-20 h-20  flex items-center justify-center'
               >
                 <img
                   src={pfp!}
-                  alt="Profile Image"
+                  alt='Profile Image'
                   style={{
                     objectFit: "cover",
                   }}
-                  tw="w-full h-full "
+                  tw='w-full h-full '
                 />
               </div>
               <h1
@@ -68,7 +68,7 @@ export default async function GET(request: NextRequest) {
                   margin: "0px",
                   fontWeight: 900,
                 }}
-                tw="font-mono text-lg  text-white"
+                tw='font-mono text-lg  text-white'
               >
                 @{username}
               </h1>
@@ -79,7 +79,7 @@ export default async function GET(request: NextRequest) {
                   fontWeight: 400,
                   color: "rgba(255,255,255, 60)",
                 }}
-                tw="font-mono text-xs font-normal "
+                tw='font-mono text-xs font-normal '
               >
                 {ShortenName(bio, 100)}
               </p>
@@ -89,11 +89,11 @@ export default async function GET(request: NextRequest) {
               style={{
                 gap: "20px",
               }}
-              tw=" w-1/2 h-full  relative flex flex-col items-center justify-start  max-w-1/2"
+              tw=' w-1/2 h-full  relative flex flex-col items-center justify-start  max-w-1/2'
             >
-              <span tw="text-lg text-white font-normal">
+              <span tw='text-lg text-white font-normal'>
                 Followers
-                <span tw="text-[#7F5FC6] font-semibold ml-2">
+                <span tw='text-[#7F5FC6] font-semibold ml-2'>
                   {follower_count >= 1000
                     ? `${Number(follower_count / 1000).toFixed(2)}k`
                     : follower_count}
@@ -101,9 +101,9 @@ export default async function GET(request: NextRequest) {
               </span>
 
               {/* following count */}
-              <span tw="text-lg text-white font-normal">
+              <span tw='text-lg text-white font-normal'>
                 Following
-                <span tw="text-[#7F5FC6] font-semibold ml-2">
+                <span tw='text-[#7F5FC6] font-semibold ml-2'>
                   {following_count >= 1000
                     ? `${Number(following_count / 1000).toFixed(2)}k`
                     : following_count}
@@ -112,7 +112,7 @@ export default async function GET(request: NextRequest) {
             </div>
           </div>
           <div
-            tw="flex w-full items-start justify-center flex-col  "
+            tw='flex w-full items-start justify-center flex-col  '
             style={{
               gap: "10px",
             }}
@@ -122,22 +122,22 @@ export default async function GET(request: NextRequest) {
                 margin: 0,
                 color: "white",
               }}
-              tw="text-lg"
+              tw='text-lg'
             >
-              TOP NFT's :
+              TOP NFTs :
             </h1>
             <div
-              tw=" flex justify-start items-center w-full"
+              tw=' flex justify-start items-center w-full'
               style={{
                 gap: 40,
               }}
             >
               {nfts.length === 0 ? (
-                <div tw="flex flex-col items-center justify-center gap-2">
-                  <span tw="text-lg font-normal text-[rgba(255,255,255,0.8)]">
+                <div tw='flex flex-col items-center justify-center gap-2'>
+                  <span tw='text-lg font-normal text-[rgba(255,255,255,0.8)]'>
                     No NFTs on Base yet
                   </span>
-                  <span tw="text-[rgba(255,255,255,0.8)]">
+                  <span tw='text-[rgba(255,255,255,0.8)]'>
                     (Check your FC connected wallet)
                   </span>
                 </div>
@@ -145,23 +145,23 @@ export default async function GET(request: NextRequest) {
                 <>
                   {nfts.map((nft: TNFTs, id: number) => (
                     <Link
-                      tw="flex flex-col items-center justify-start "
+                      tw='flex flex-col items-center justify-start '
                       key={id}
                       style={{
                         marginLeft: 10,
                         gap: 10,
                       }}
                       href={nft.nftUrl}
-                      target="_blank"
+                      target='_blank'
                     >
                       {/* nft img */}
                       <img
                         src={nft.imageUrl}
-                        alt=""
-                        tw="w-16 h-16 rounded-2xl"
+                        alt=''
+                        tw='w-16 h-16 rounded-2xl'
                       />
                       {/* nft icon */}
-                      <span tw=" text-[10px] md:text-xs  text-[rgba(255,255,255,0.8)]  font-normal">
+                      <span tw=' text-[10px] md:text-xs  text-[rgba(255,255,255,0.8)]  font-normal'>
                         {/* ALLOW ONLY 15 CHARACTERS FOR NFT NAME ELSE ...... */}
                         {ShortenName(nft.name, 15)}
                       </span>
